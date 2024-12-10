06a8dd2bc41b3976872fd4c1c2e5612f6206e2b1
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-10 23:57:13 +0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, int) {
+  if (int < 0) {
+    return "ERROR";
+  } else {
+    let result = "";
+    for (i = 0; i < int; i++) {
+      result += str;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
