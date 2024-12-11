72a180610fdfcc1c968ae87c8251d761221f5c0f
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-11 22:47:04 +0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (start, stop) {
+  let sum = 0;
+  let first = start;
+  let second = stop;
+  if (stop < start) {
+    first = stop;
+    second = start;
+  } else if ((start || stop) < 0) {
+    return "ERROR";
+  } else if (typeof start === "number" && start % 1 !== 0) {
+    return "ERROR";
+  } else if (typeof stop === "string") {
+    return "ERROR";
+  } else if (stop instanceof Array) {
+    return "ERROR";
+  }
+  for (let i = first; i <= second; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
