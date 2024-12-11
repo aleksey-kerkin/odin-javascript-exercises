d78724a5caa395b94be80c1f8c06e2d93cb00d58
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-11 23:08:15 +0500

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let temperatureInC = (temp - 32) * (5 / 9);
+  return Number(temperatureInC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let temperatureInF = temp * (9 / 5) + 32;
+  return Number(temperatureInF.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
