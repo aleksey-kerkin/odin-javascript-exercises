421e54796bf814904320706acf30e274ad6314cf
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-24 00:51:16 +0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, currentNumber) => total + currentNumber, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, currentNumber) => total * currentNumber, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let sum = 1;
+    for (i = 2; i <= num; i++) {
+      sum = sum * i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
