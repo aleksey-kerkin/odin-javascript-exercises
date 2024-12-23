50c5f7c686bab6bf01dd2319730c9b0c718ae102
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-24 01:19:10 +0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let string = str.toLowerCase().split(/\W/).join("");
+  let reversedString = string.split("").reverse().join("");
+  return reversedString === string;
 };
 
 // Do not edit below this line
