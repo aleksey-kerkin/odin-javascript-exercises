a620d28499eb9b163319f760f9f336f699e00c03
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-24 19:51:10 +0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const oldest = arr.sort((a, b) => {
+    if (!a.yearOfDeath) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    if (!b.yearOfDeath) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
