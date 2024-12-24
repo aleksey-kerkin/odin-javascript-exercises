8e15eed419f63182c74cc5d90b1c704aa63b36fc
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-24 19:32:02 +0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  const fib = [0, 1];
+  const count = parseInt(num);
+  for (let i = 2; i <= count; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[count];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
